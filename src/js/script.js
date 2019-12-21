cce4858873123e1291dd2266ea98deea578093c9
Jakub Folta <jakubfolta@yahoo.co.uk>
2019-12-21 16:23:41 +0000

    Continue course - adding jQuery.

--- src/js/script.js
+++ src/js/script.js
@@ -21,5 +21,33 @@ $(document).ready(function() {
     $('html, body').animate({scrollTop: $('.js--section-features').offset().top}, 1000)
   });
 
+  // Navigation scroll
+  // Select all links with hashes
+  $('a[href*="#"]')
+    // Remove links that don't actually link to anything
+    .not('[href="#"]')
+    .not('[href="#0"]')
+    .click(function(event) {
+      // On-page links
+      if (
+        location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+        &&
+        location.hostname == this.hostname
+      ) {
+        // Figure out element to scroll to
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+        // Does a scroll target exist?
+        if (target.length) {
+          // Only prevent default if animation is actually gonna happen
+          event.preventDefault();
+          $('html, body').animate({
+            scrollTop: target.offset().top
+          }, 1000);
+          return False;
+        }
+      }
+    });
+
 
 });
