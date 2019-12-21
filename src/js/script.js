f7cdbca4a3065b1b4b1edd076035b72108d21b95
Jakub Folta <jakubfolta@yahoo.co.uk>
2019-12-21 17:09:03 +0000

    Continue course - adding animations.

--- src/js/script.js
+++ src/js/script.js
@@ -49,5 +49,9 @@ $(document).ready(function() {
       }
     });
 
+  // Animations on scroll
+  $('.js--wp-1').waypoint(function(direction) {
+    $('.js--wp-1').addClass('animated fadeIn');
+  })
 
 });
