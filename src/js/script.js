fbae76b33da4ab06d452faf2bbaa0209eafa771e
Jakub Folta <jakubfolta@yahoo.co.uk>
2019-12-21 16:00:26 +0000

    Continue course - adding jQuery.

--- src/js/script.js
+++ src/js/script.js
@@ -13,5 +13,9 @@ $(document).ready(function() {
 
 
   // Scroll on buttons
+  $('.js--scroll-to-plans').click(function() {
+    $('html, body').animate({scrollTop: $('.js--section-plans')})
+  })
+
 
 });
