b4ba23653ae93b511057d230e909b9767c9bf0e8
Jakub Folta <jakubfolta@yahoo.co.uk>
2019-12-23 17:09:54 +0000

    Add responsive nav using jQuery.

--- src/js/script.js
+++ src/js/script.js
@@ -73,4 +73,22 @@ $(document).ready(function() {
     offset: '50%'
   })
 
+  // Mobile nav
+  $('.js--nav-icon').click(function() {
+    var nav = $('.js--main-nav');
+    var icon = $('.js--nav-icon i');
+
+    if(icon.hasClass('ion-md-menu')) {
+      icon.addClass('ion-md-close')
+      icon.removeClass('ion-md-menu')
+    } else{
+      icon.addClass('ion-md-menu')
+      icon.removeClass('ion-md-close')
+    }
+
+    nav.slideToggle(200)
+  })
+
 });
+
+
